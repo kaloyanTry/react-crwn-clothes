b48687c4acdc9f776afbd331291c1a4e222b0b94
kaloyanTry <kaloyantryavna@gmail.com>
2023-04-07 21:29:55 +0300

    Adding home route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path='/home' element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
