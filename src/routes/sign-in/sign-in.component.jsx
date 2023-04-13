63f0729e2465f51ea99f046f283ee76d237d888f
kaloyanTry <kaloyantryavna@gmail.com>
2023-04-13 09:19:10 +0300

    Added firebase auth user

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,12 @@
-import { signInWithGooglePopup } from '../../utils/firebase/farebase.utils';
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/farebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
   return (
     <div>
--- src/utils/firebase/farebase.utils.js
+++ src/utils/firebase/farebase.utils.js
@@ -33,8 +33,23 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
-  console.log(userDocRef);
 
-  const userSnapShot = await getDoc(userDocRef);
-  console.log(userSnapShot);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
 };
