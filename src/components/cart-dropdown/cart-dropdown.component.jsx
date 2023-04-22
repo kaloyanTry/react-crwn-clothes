b96c59fbb255a6e3d983a41b5742155b63119cfd
kaloyanTry <kaloyantryavna@gmail.com>
2023-04-22 15:23:11 +0300

    Added shoping cart and calculate the total price, checkout page and items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -7,6 +9,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
   return (
     <div className='cart-dropdown-container'>
@@ -15,7 +22,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -8,7 +8,7 @@ const CartItem = ({ cartItem }) => {
       <div className='item-details'>
         <span className='name'>{name}</span>
         <span className='price'>
-          {quantity} * ${price}
+          {quantity} * €{price}
         </span>
       </div>
     </div>
