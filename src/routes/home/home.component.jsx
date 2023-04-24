c1bdaf8bce2a10959dd987c4f4289a348c0ea7d9
kaloyanTry <kaloyantryavna@gmail.com>
2023-04-24 08:49:32 +0300

    Added scss styles and functioniality

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
-  const categorires = [
+  const categories = [
     {
       id: 1,
       title: 'hats',
@@ -33,7 +33,7 @@ const Home = () => {
 
   return (
     <div>
-      <Directory categories={categorires} />
+      <Directory categories={categories} />
       <Outlet />
     </div>
   );
