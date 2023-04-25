ce65d2be4b5649304a7632f0edf893098ac67b81
kaloyanTry <kaloyantryavna@gmail.com>
2023-04-25 17:22:23 +0300

    Added jsx instead scss functionality

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,14 +1,15 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-import './authintication.styles.scss';
+import './authintication.styles.jsx';
+import { AuthenticationContainer } from './authintication.styles.jsx';
 
 const Authentication = () => {
   return (
-    <div className='authentication-container'>
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
