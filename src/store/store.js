d8b39a78b16ef7672510ebe8c563c737d942623f
kaloyanTry <kaloyantryavna@gmail.com>
2023-05-05 21:16:12 +0300

    Added root reducer store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,15 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { compose, legacy_createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
-// root-reducer
+import { rootReducer } from './root-reducer';
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = legacy_createStore(
+  rootReducer,
+  undefined,
+  composedEnhancers
+);
