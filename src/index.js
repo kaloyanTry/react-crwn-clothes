f580c6455f181a37f73325f2bf8362e824aea4a4
kaloyanTry <kaloyantryavna@gmail.com>
2023-05-11 21:16:08 +0300

    Added evth before redux toolkit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
 import { store, persistor } from './store/store';
-
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
